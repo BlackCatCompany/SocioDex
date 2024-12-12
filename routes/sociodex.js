76e143bc8ffd5a8c573da479650be7f23d637b0e
Wladison-Maciel <franciscowladison97@gmail.com>
2024-12-12 17:05:17 -0300

    Puxando Filosofos para a tela Sociodex

--- routes/sociodex.js
+++ routes/sociodex.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('sociodex', { title: 'Express' });
+// Modelo Sociologo (Certifique-se de que o modelo está correto)
+const Sociologo = mongoose.model('Sociologo', new mongoose.Schema({
+  nome: String
+}), 'ListaSociologos')
+
+// Rota para exibir os sociólogos
+router.get('/', (req, res) => {
+  Sociologo.find() // Busca todos os sociólogos
+    .then(sociologos => {
+      // Passa os dados para o EJS
+      res.render('sociodex', { sociologos: sociologos }); 
+    })
+    .catch(err => {
+      console.error('Erro ao buscar sociólogos', err);
+      res.status(500).send('Erro ao buscar sociólogos');
+    });
 });
 
 module.exports = router;
