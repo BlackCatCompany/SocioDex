2d2985bde9bad049ab34ace289a73fa53e32f0f5
Francisco Wladison <franciscowladison97@gmail.com>
2024-12-23 10:41:14 -0300

    Adicionando linha do tempo na index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const timelineData = [
+  {
+    date: "1838",
+    event: "Criação do termo 'Sociologia'",
+    description: "Auguste Comte introduz o termo 'sociologia' em sua obra 'Curso de Filosofia Positiva'.",
+    icon: "/public/surgimento.jpg"
+  },
+  {
+    date: "1897",
+    event: "Publicação de 'O Suicídio'",
+    description: "Émile Durkheim analisa o suicídio como um fenômeno social em seu estudo clássico.",
+    icon: "/icons/durkheim.png"
+  },
+  {
+    date: "1904",
+    event: "Publicação de 'A Ética Protestante e o Espírito do Capitalismo'",
+    description: "Max Weber explora a relação entre religião e capitalismo em uma obra seminal.",
+    icon: "/icons/weber.png"
+  }
+];
+
+router.get('/', (req, res) => {
+  res.render('index', { timeline: timelineData });
 });
 
 module.exports = router;
