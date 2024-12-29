818008c9cb921aabaffde109d26975d88f76e646
Francisco Wladison <franciscowladison97@gmail.com>
2024-12-29 17:43:10 -0300

    Globo Finalizado com informações no backend

--- routes/socioGlobo.js
+++ routes/socioGlobo.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+// Dados das marcações no mapa
+const locations = [
+    {
+        name: 'Fortaleza, Brazil',
+        coordinates: [-38.5426, -3.71722],
+        image: '/images/augustecomte.jpg'
+    },
+    {
+        name: 'São Paulo, Brazil',
+        coordinates: [-46.6333, -23.5505],
+        image: '/images/Durkheim.jpg'
+    },
+    {
+        name: 'Rio de Janeiro, Brazil',
+        coordinates: [-43.1729, -22.9068],
+        image: '/images/darwin.jpg'
+    }
+];
+
+// Rota para renderizar o EJS e passar os dados das marcações
 router.get('/', (req, res) => {
-    res.render('socioGlobo');
+    res.render('socioGlobo', { locations });
 });
 
 module.exports = router;
