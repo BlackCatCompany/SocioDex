4aadeb541aa344a9052a6cc0a3394536f7799b4a
Francisco Wladison <franciscowladison97@gmail.com>
2025-01-03 11:48:10 -0300

    Adicionando função para adcionar perguntas

--- routes/adicionarPerguntas.js
+++ routes/adicionarPerguntas.js
@@ -1,12 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Pergunta = require("../models/perguntas"); // Modelo de Perguntas
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  try{
-    res.render('adicionarPerguntas');
-  }catch(error){
-    res.status(500).send('Erro ao carregar página de adicionar perguntas: ' + error.message);
+// Página para adicionar perguntas
+router.get("/", (req, res) => {
+  res.render("adicionarPerguntas", { title: "Adicionar Pergunta" });
+});
+
+// Adicionar uma nova pergunta ao banco de dados
+router.post("/adicionar", async (req, res) => {
+  const { pergunta, alternativas, respostaCorreta } = req.body;
+
+  try {
+    const novaPergunta = new Pergunta({
+      pergunta,
+      alternativas: alternativas.split(","),
+      respostaCorreta,
+    });
+    await novaPergunta.save();
+    console.log("Pergunta adicionada: "+ novaPergunta)
+    res.redirect("/adicionarPerguntas");
+  } catch (error) {
+    res.status(500).send("Erro ao adicionar pergunta: " + error.message);
   }
 });
 
